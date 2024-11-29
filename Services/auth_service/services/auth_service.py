18e7757de9c038368c2b0eda8f79c90e14f795c4
B Anbuselvan <111638242+Anbuselvan2114@users.noreply.github.com>
2024-11-29 10:45:09 +0530

    Delete Services directory

--- Services/auth_service/services/auth_service.py
+++
@@ -1,8 +0,0 @@
-
-class authService():
-    def __init__(self):
-        self.users = {"admin": "password123"}
-
-    def get_auth(self, Username: str) -> dict:
-        return self.users.get(Username)
-
