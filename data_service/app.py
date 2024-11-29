e8ec6b4471b7b985c1f537feb3ba17cc310666ff
B Anbuselvan <111638242+Anbuselvan2114@users.noreply.github.com>
2024-11-29 10:44:36 +0530

    Delete data_service directory

--- data_service/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from user_routes import user_blueprint  # Import the blueprint
-
-app = Flask(__name__)
-app.register_blueprint(user_blueprint, url_prefix='/data')  # Register with a URL prefix
-
-if __name__ == "__main__":
-    app.run(debug=True)
